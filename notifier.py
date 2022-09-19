7567de3f73550d05911837c664cdb88d9ccb667e
samighumay <87988704+samighumay@users.noreply.github.com>
2022-09-19 21:02:30 +0600

    Add files via upload

---
+++ notifier.py
@@ -0,0 +1,12 @@
+from email import message
+import time
+from win10toast import ToastNotifier
+
+while True:
+    toaster = ToastNotifier()
+    time.sleep(2)
+    toaster.show_toast("Hey Kid", "Lol you are single", duration=12, threaded=True)
+    
+while toaster.notification_active:
+    time.sleep(0.2)
+
