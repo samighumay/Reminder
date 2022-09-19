eacfdeedc15cea44da507288023f3d1ed59f49b3
samighumay <87988704+samighumay@users.noreply.github.com>
2022-09-19 21:04:28 +0600

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -3,7 +3,7 @@ from win10toast import ToastNotifier
 
 while True:
     toaster = ToastNotifier()
-    time.sleep(2)
+    time.sleep(69*60)
     toaster.show_toast("Hey Kid", "Lol you are single", duration=12, threaded=True)
     
 while toaster.notification_active:
